4a3285f488480cf86d6384cd6bcab11b19be63a0
tanishq53 <work.tanishqgautam15@gmail.com>
2026-07-26 09:02:29 +0530

    again firebase

--- frontend/notes/src/firebase.js
+++ frontend/notes/src/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// Optional: only keep Analytics if you really want it
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDBm2WU5SSr3kn5mey1qoN9tiq8RkejDo0",
   authDomain: "second-brain-edc31.firebaseapp.com",
@@ -13,9 +10,16 @@ const firebaseConfig = {
   storageBucket: "second-brain-edc31.firebasestorage.app",
   messagingSenderId: "109051647806",
   appId: "1:109051647806:web:438722624c331b6f2b3e6f",
-  measurementId: "G-WKHZ6GGNQW"
+  measurementId: "G-WKHZ6GGNQW",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Optional
 const analytics = getAnalytics(app);
+
+// ⭐ These are what your App.jsx needs
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
+export default app;
