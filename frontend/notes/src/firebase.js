bf8fdc30dde920a2ab8e67e617e9c99e36e25d9a
tanishq53 <work.tanishqgautam15@gmail.com>
2026-07-26 10:04:03 +0530

    google auth

--- frontend/notes/src/firebase.js
+++ frontend/notes/src/firebase.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics, isSupported } from "firebase/analytics";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDBm2WU5SSr3kn5mey1qoN9tiq8RkejDo0",
   authDomain: "second-brain-edc31.firebaseapp.com",
@@ -14,19 +15,14 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-// Analytics (optional)
 isSupported().then((supported) => {
   if (supported) {
     getAnalytics(app);
   }
 });
-const app = initializeApp(firebaseConfig);
-
-isSupported().then((supported) => {
-  if (supported) getAnalytics(app);
-});
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+
 export default app;
